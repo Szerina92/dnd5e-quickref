ac491a42885a4563a67e01e3fcacc7959c93313b
Szerina <161936340+Szerina92@users.noreply.github.com>
2025-02-18 11:00:29 +0100

    Update data_environment.js

--- js/data_environment.js
+++ js/data_environment.js
@@ -81,6 +81,17 @@ data_environment_vision = [
         ]
     },
     {
+        title: "Erschütterungssinn",
+        icon: "sense",
+        subtitle: "Spüre Erschütterungen",
+        description: "Erschütterungssinn nimmt bewegungen auf einer Oberfläche wahr",
+        reference: "PHB 2024",
+        bullets: [
+            "A creature with Tremorsense can pinpoint the location of creatures and moving objects within a specific range, provided that the creature with Tremorsense and anything it is detecting are both in contact with the same surface (such as the ground, a wall, or a ceiling) or the same liquid.",
+	    "Tremorsense can't detect creatures or objects in the air, and it doesn't count as a form of sight."
+        ]
+    },
+    {
         title: "Wahrer Blick",
         icon: "eye-shield",
         subtitle: "Alles sehen",
