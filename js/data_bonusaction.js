c94938dec8e078ccdadb9afebfe13974591bc32d
Szerina <161936340+Szerina92@users.noreply.github.com>
2025-02-18 09:51:16 +0100

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -4,33 +4,42 @@ data_bonusaction = [
         icon: "crossed-swords",
         subtitle: "Nur mit Angriffsaktion",
         description: "Greife mit deiner zweiten Waffe an",
-        reference: "PHB, Seiten 192,194-195.",
+        reference: "PHB 2024",
         bullets: [
-            "Nur nutzbar, wenn du die <i>Angriff</i>-Aktion mit einer leichten Nahkampfwaffe durchführst.",
-			"Führe einen einzelnen Angriff mit einer anderen leichten Nahkampfwaffe, die du in der anderen Hand hältst, aus.",
-            "Du addierst deinen Fähigkeitenmodifikator nicht auf den Schaden des Angriffs mit der Zweitwaffe, es sei denn, der Modifikator ist negativ.",
+            "Nur nutzbar, wenn du die <i>Angriff</i> Aktion mit einer leichten Nahkampfwaffe durchführst.",
+	    "Führe einen einzelnen Angriff mit einer anderen leichten Nahkampfwaffe, die du in der anderen Hand hältst, aus.",
+            "Du addierst deinen Attributsmodifikator nicht auf den Schaden des Angriffs mit der Zweitwaffe, es sei denn, der Modifikator ist negativ.",
             "Wenn eine der Waffen die geworfen Eigenschaft hat, kannst du die Waffe werfen anstatt einen Nahkampfangriff zu machen."
         ]
     },
     {
-        title: "Zauberwirken",
+        title: "Magie",
         icon: "magic-swirl",
         subtitle: "Zauberzeit: 1 Bonusaktion",
-        description: "Wirke einen Zauberspruch mit einer Zauberzeit von 1 Bonusaktion",
-        reference: "PHB, Seite 193.",
+        description: "Wirke einen Zauber mit einer Zauberzeit von 1 Bonusaktion",
+        reference: "PHB 2024",
         bullets: [
-            "Du kannst mit deiner Aktion keinen Zauberspruch wirken, während du gleichzeitig einen anderen Zauberspruch mit deiner Bonusaktion wirkst, es sei denn, die Aktion wird für einen Zaubertrick verwendet.",
-            "Für Details siehe die <i>Zauberwirken</i> Aktion."
+            "Some spells that have a casting time of a Bonus Action are also cast in response to a trigger defindet in the spell",
+            "Für Details siehe die <i>Magie</i> Aktion."
         ]
     },
     {
+        title: "Tränke",
+        icon: "potion",
+        subtitle: "Trinke einen Trank",
+        description: "Tränke sind Verbrauchsgegenstände",
+        reference: "PHB 2024",
+        bullets: [
+		"Drinking a potion or administering it to another creature requires a Bonus Action. Applying an oil might take longer as specified in its description. Once used, a potion takes effect immediately, and its used up."
+        ]
+    },	
+    {
         title: "Nutze ein Merkmal",
         icon: "embrassed-energy",
-        subtitle: "Volks-/Klassenmerkmal",
-        description: "Nutze ein Volks- oder Klassenmerkmal, das eine Bonusaktion verbraucht",
+        subtitle: "Spezies-/Klassenmerkmal",
+        description: "Nutze ein Spezies- oder Klassenmerkmal, das eine Bonusaktion verbraucht",
         reference: "Siehe die Beschreibung des Merkmals",
         bullets: [
-
         ]
     }
 ]
