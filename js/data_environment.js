786221d692692a0e764dd3fdb0095d5195d97466
Szerina <161936340+Szerina92@users.noreply.github.com>
2025-02-18 16:14:31 +0100

    Update data_environment.js

--- js/data_environment.js
+++ js/data_environment.js
@@ -82,13 +82,13 @@ data_environment_vision = [
     },
     {
         title: "Erschütterungssinn",
-        icon: "sense",
+        icon: "erschuetterungssinn",
         subtitle: "Spüre Erschütterungen",
         description: "Erschütterungssinn nimmt bewegungen auf einer Oberfläche wahr",
         reference: "PHB 2024",
         bullets: [
-            "A creature with Tremorsense can pinpoint the location of creatures and moving objects within a specific range, provided that the creature with Tremorsense and anything it is detecting are both in contact with the same surface (such as the ground, a wall, or a ceiling) or the same liquid.",
-	    "Tremorsense can't detect creatures or objects in the air, and it doesn't count as a form of sight."
+            "Eine Kreatur mit Erschütterungssinn kann die Position von Kreaturen und sich bewegenden Objekten innerhalb einer bestimmten Reichweite bestimmen, vorausgesetzt, dass die Kreatur mit Erschütterungssinn und alles, was sie erfasst, mit derselben Oberfläche (z. B. dem Boden, einer Wand oder einer Decke) oder derselben Flüssigkeit in Kontakt sind.",
+	    "Erschütterungssinn kann keine Kreaturen oder Objekte in der Luft erkennen und zählt nicht als eine Form des Sehens."
         ]
     },
     {
@@ -144,63 +144,57 @@ data_environment_cover = [
 
 data_environment_hazards = [
   {
-        title: "Burning",
+        title: "Brennen",
         icon: "flame",
-        subtitle: " ",
-        description: "Ein Ziel hat Teildeckung wenn ein Hindernis mindestens die Hälfte seines Körpers verdeckt",
-        reference: "PHB, Seiten 195-196.",
+        subtitle: "In Brand geraten",
+        description: "Eine Kreatur oder ein Objekt brennt",
+        reference: "PHB 2024",
         bullets: [
-            "Das Hindernis kann zB eine niedrige Mauer, ein großes Möbelstück, ein schmaler Baumstamm oder eine Kreatur (unabhängig ob sie freundlich oder feindlich ist) sein.",
-			"Ein Ziel mit Teildeckung erhält einen <b>+2 Bonus auf die Rüstungsklasse und Proben auf Geschicklichkeit</b>.",
-            "Wenn ein Ziel sich hinter mehreren Quellen von Deckung befindet, wird nur die größte Deckung angewandt"
+            "A burning creature or object takes 1d4 Fire damage at the start of each of its turns. As an action, you can extinguish fire on yourself by giving yourself the Prone condition and rolling on the ground. The fire also goes out if it is doused, submerged, or suffocated."
         ]
     },
     {
-        title: "Dehydration",
+        title: "Dehydrierung",
         icon: "drop",
-        subtitle: " ",
-        description: "Ein Ziel hat Teildeckung wenn ein Hindernis mindestens die Hälfte seines Körpers verdeckt",
-        reference: "PHB, Seiten 195-196.",
+        subtitle: "Verdursten",
+        description: "Die meisten Kreaturen brauchen Wasser",
+        reference: "PHB 2024",
         bullets: [
-            "Das Hindernis kann zB eine niedrige Mauer, ein großes Möbelstück, ein schmaler Baumstamm oder eine Kreatur (unabhängig ob sie freundlich oder feindlich ist) sein.",
-			"Ein Ziel mit Teildeckung erhält einen <b>+2 Bonus auf die Rüstungsklasse und Proben auf Geschicklichkeit</b>.",
-            "Wenn ein Ziel sich hinter mehreren Quellen von Deckung befindet, wird nur die größte Deckung angewandt"
+            "<p>A creature requires an amount of water per day based on its size, as shown in the Water Needs per Day table. A creature that drinks less than half the required water for a day gains 1 Exhaustion level at the day’s end. Exhaustion caused by dehydration can’t be removed until the creature drinks the full amount of water required for a day.",
+	    "</p><table><caption>Water Needs per Day</caption><thead><tr><th><p>Size</p></th><th><p>Water</p></th></tr></thead><tbody><tr><td><p>Tiny</p></td><td><p>1/4 gallon</p></td></tr><tr><td><p>Small</p></td><td><p>1 gallon</p></td></tr><tr><td><p>Medium</p></td><td><p>1 gallon</p></td></tr><tr><td><p>Large</p></td><td><p>4 gallons</p></td></tr><tr><td><p>Huge</p></td><td><p>16 gallons</p></td></tr><tr><td><p>Gargantuan</p></td><td><p>64 gallons</p></td></tr></tbody></table>"
         ]
     },
     {
-        title: "Falling",
-        icon: "fall",
-        subtitle: " ",
-        description: "Ein Ziel hat Teildeckung wenn ein Hindernis mindestens die Hälfte seines Körpers verdeckt",
-        reference: "PHB, Seiten 195-196.",
+        title: "Fallen",
+        icon: "falling",
+        subtitle: "Ungebremst fallen",
+        description: " ",
+        reference: "PHB 2024",
         bullets: [
-            "Das Hindernis kann zB eine niedrige Mauer, ein großes Möbelstück, ein schmaler Baumstamm oder eine Kreatur (unabhängig ob sie freundlich oder feindlich ist) sein.",
-			"Ein Ziel mit Teildeckung erhält einen <b>+2 Bonus auf die Rüstungsklasse und Proben auf Geschicklichkeit</b>.",
-            "Wenn ein Ziel sich hinter mehreren Quellen von Deckung befindet, wird nur die größte Deckung angewandt"
+            "A creature that falls takes 1d6 Bludgeoning damage at the end of the fall for every 10 feet it fell, to a maximum of 20d6. When the creature lands, it has the Prone condition unless it avoids taking any damage from the fall.",
+	    "A creature that falls into water or another liquid can use its Reaction to make a DC 15 Strength (Athletics) or Dexterity (Acrobatics) check to hit the surface head or feet first. On a successful check, any damage resulting from the fall is halved.",
         ]
     },
     {
-        title: "Malnutrition",
-        icon: "food",
-        subtitle: " ",
+        title: "Mangelernährung",
+        icon: "mangelernaehrung",
+        subtitle: "Die meisten Kreaturen brauchen Nahrung",
         description: "Ein Ziel hat Teildeckung wenn ein Hindernis mindestens die Hälfte seines Körpers verdeckt",
-        reference: "PHB, Seiten 195-196.",
+        reference: "PHB 2024",
         bullets: [
-            "Das Hindernis kann zB eine niedrige Mauer, ein großes Möbelstück, ein schmaler Baumstamm oder eine Kreatur (unabhängig ob sie freundlich oder feindlich ist) sein.",
-			"Ein Ziel mit Teildeckung erhält einen <b>+2 Bonus auf die Rüstungsklasse und Proben auf Geschicklichkeit</b>.",
-            "Wenn ein Ziel sich hinter mehreren Quellen von Deckung befindet, wird nur die größte Deckung angewandt"
+            "A creature needs an amount of food per day based on its size, as shown in the Food Needs per Day table. A creature that eats but consumes less than half the required food for a day must succeed on a DC 10 Constitution saving throw or gain 1 Exhaustion level at the day’s end. A creature that eats nothing for 5 days automatically gains 1 Exhaustion level at the end of the fifth day as well as an additional level at the end of each subsequent day without food.",
+	    "Exhaustion caused by malnutrition can’t be removed until the creature eats the full amount of food required for a day. See also “Exhaustion.”",
+            "<table><caption>Food Needs per Day</caption><thead><tr><th><p>Size</p></th><th><p>Food</p></th><th></th><th><p>Size</p></th><th><p>Food</p></th></tr></thead><tbody><tr><td><p>Tiny</p></td><td><p>1/4 pound</p></td><td></td><td><p>Large</p></td><td><p>4 pounds</p></td></tr><tr><td><p>Small</p></td><td><p>1 pound</p></td><td></td><td><p>Huge</p></td><td><p>16 pounds</p></td></tr><tr><td><p>Medium</p></td><td><p>1 pound</p></td><td></td><td><p>Gargantuan</p></td><td><p>64 pounds</p></td></tr></tbody></table>"
         ]
     },
     {
-        title: "Suffocation",
-        icon: "wind",
-        subtitle: " ",
-        description: "Ein Ziel hat Teildeckung wenn ein Hindernis mindestens die Hälfte seines Körpers verdeckt",
-        reference: "PHB, Seiten 195-196.",
+        title: "Ersticken",
+        icon: "ersticken",
+        subtitle: "Nach Luft ringen",
+        description: "Die meisten Kreaturen brauchen Luft",
+        reference: "PHB 2024",
         bullets: [
-            "Das Hindernis kann zB eine niedrige Mauer, ein großes Möbelstück, ein schmaler Baumstamm oder eine Kreatur (unabhängig ob sie freundlich oder feindlich ist) sein.",
-			"Ein Ziel mit Teildeckung erhält einen <b>+2 Bonus auf die Rüstungsklasse und Proben auf Geschicklichkeit</b>.",
-            "Wenn ein Ziel sich hinter mehreren Quellen von Deckung befindet, wird nur die größte Deckung angewandt"
+            "A creature can hold its breath for a number of minutes equal to 1 plus its Constitution modifier (minimum of 30 seconds) before suffocation begins. When a creature runs out of breath or is choking, it gains 1 Exhaustion level at the end of each of its turns. When a creature can breathe again, it removes all levels of Exhaustion it gained from suffocating."
         ]
     }
 ]
