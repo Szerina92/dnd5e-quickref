75f7df403993fac955d60b800871b59a82d98844
Szerina <161936340+Szerina92@users.noreply.github.com>
2025-02-18 17:49:56 +0100

    Create data_coremechanics.js

---
+++ js/data_coremechanics.js
@@ -0,0 +1,37 @@
+data_coremechanics = [
+  {
+        title: "Teildeckung",
+        icon: "broken-shield",
+        subtitle: "Niedr. Mauer, Möbel, Kreatur",
+        description: "Ein Ziel hat Teildeckung wenn ein Hindernis mindestens die Hälfte seines Körpers verdeckt",
+        reference: "PHB, Seiten 195-196.",
+        bullets: [
+            "Das Hindernis kann zB eine niedrige Mauer, ein großes Möbelstück, ein schmaler Baumstamm oder eine Kreatur (unabhängig ob sie freundlich oder feindlich ist) sein.",
+			"Ein Ziel mit Teildeckung erhält einen <b>+2 Bonus auf die Rüstungsklasse und Proben auf Geschicklichkeit</b>.",
+            "Wenn ein Ziel sich hinter mehreren Quellen von Deckung befindet, wird nur die größte Deckung angewandt"
+        ]
+    },
+    {
+        title: "Dreivierteldeckung",
+        icon: "cracked-shield",
+        subtitle: "Fallgatter, Pfeilschlitze",
+        description: "Ein Ziel hat Dreivierteldeckung wenn ein Hindernis ungefähr drei Viertel seines Körpers verdeckt",
+        reference: "PHB, Seiten 195-196.",
+        bullets: [
+            "Das Hindernis kann zB ein Fallgatter, ein Pfeilschlitz oder ein dicker Baumstamm sein.",
+			"Ein Ziel mit Dreivierteldeckung erhält einen <b>+5 Bonus auf die Rüstungsklasse und Proben auf Geschicklichkeit</b>.",
+            "Wenn ein Ziel sich hinter mehreren Quellen von Deckung befindet, wird nur die größte Deckung angewandt"
+        ]
+    },
+    {
+        title: "Volle Deckung",
+        icon: "shield",
+        subtitle: "Komplett verdeckt",
+        description: "Ein Ziel hat Volle Deckung wenn es durch ein Hindernis vollständig verdeckt wird",
+        reference: "PHB, Seiten 195-196.",
+        bullets: [
+            "Ein Ziel hinter voller Deckung <b>kann nicht direkt als Ziel für einen Angriff oder Zauberspruch gewählt werden</b>, wobei manche Zauber ein solches Ziel innerhalb eines Effektbereichs treffen können.",
+            "Wenn ein Ziel sich hinter mehreren Quellen von Deckung befindet, wird nur die größte Deckung angewandt"
+        ]
+    }
+  ]
